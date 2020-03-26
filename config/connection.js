bcd17df53a7938d7e8577872f305fce080acc6db
Jordon <Jordonrunge20@gmail.com>
2020-03-26 01:05:13 -0500

    Trying to launch on Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "pure-lake-56171.herokuapp.com",
+  host: "localhost",
   port: 3306,
   user: "root",
   password: "Supertomy11",
